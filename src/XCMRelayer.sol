8e18114ba343168ab46d88fbcbff63721f3cae67
nuno <hi@nunoalexandre.com>
2023-09-13 17:01:17 +0200

    Encode sourceAddress hex string to bytes
    So for example 0x8503b4452Bf6238cC76CdbEE223b46d7196b1c93 is encoded as
    307838353033623434353242663632333863433736436462454532323362343664373139366231633933
    (it includes the 0x prefix)

--- src/XCMRelayer.sol
+++ src/XCMRelayer.sol
@@ -299,7 +299,7 @@ function _processMsgPayload(string memory sourceChain, string memory sourceAddre
     returns (bytes memory)
 {
     bytes memory sourceChainBytes = bytes(sourceChain);
-    bytes memory sourceAddressBytes = fromHex(sourceAddress);
+    bytes memory sourceAddressBytes = bytes(sourceAddress);
 
     return abi.encodePacked(
         uint32(sourceChainBytes.length),
@@ -308,30 +308,4 @@ function _processMsgPayload(string memory sourceChain, string memory sourceAddre
         sourceAddressBytes,
         message
     );
-}
-
-// Convert an hexadecimal character to their byte value
-function fromHexChar(uint8 c) pure returns (uint8) {
-    if (bytes1(c) >= bytes1("0") && bytes1(c) <= bytes1("9")) {
-        return c - uint8(bytes1("0"));
-    }
-    if (bytes1(c) >= bytes1("a") && bytes1(c) <= bytes1("f")) {
-        return 10 + c - uint8(bytes1("a"));
-    }
-    if (bytes1(c) >= bytes1("A") && bytes1(c) <= bytes1("F")) {
-        return 10 + c - uint8(bytes1("A"));
-    }
-    revert("Failed to encode hex char");
-}
-
-// Convert an hexadecimal string to raw bytes
-function fromHex(string memory s) pure returns (bytes memory) {
-    bytes memory ss = bytes(s);
-    require(ss.length % 2 == 0); // length must be even
-    bytes memory r = new bytes(ss.length / 2);
-
-    for (uint256 i = 0; i < ss.length / 2; ++i) {
-        r[i] = bytes1(fromHexChar(uint8(ss[2 * i])) * 16 + fromHexChar(uint8(ss[2 * i + 1])));
-    }
-    return r;
 }
